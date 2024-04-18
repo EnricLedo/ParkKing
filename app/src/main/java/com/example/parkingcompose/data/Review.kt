373a01785d8525d43b97f3bc87b8ad31d90f9e4c
Raul <rfernandez@alumnat.copernic.cat>
2024-04-18 21:12:46 +0200

    REVIEWS DAO ARREGLADO Y INCLUIDO EL SORT BY RATING Y SEARCH FILTER BY RATING

--- app/src/main/java/com/example/parkingcompose/data/Review.kt
+++ app/src/main/java/com/example/parkingcompose/data/Review.kt
@@ -1,13 +1,14 @@
 package com.example.parkingcompose.data
 
 import java.util.Date
-
 data class Review(
     val id: String? = null,
-    var review_rating: Float,
-    val comment: String,
-    val title: String,
-    val user_email: String,
-    val date: Date,
-    val parking_id: String
-)
+    val title: String? = null,
+    val review_rating: Float? = null,
+    val comment: String? = null,
+    val user_email: String? = null,
+    val date: Date? = null,
+    val parking_id: Any? = null // Change the type to Any to handle both Long and String
+) {
+    constructor() : this(null, null, null, null, null, null, null)
+}
